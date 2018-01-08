8b84eb05b165045c93e3a1c664431fb49576ff51
moeabdol <mohd.a.saed@gmail.com>
2018-01-08 17:15:09 +0300

    Implement show page

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+  static get propTypes() {
+    return {
+      fetchPost: PropTypes.func,
+      match: PropTypes.object,
+      post: PropTypes.object
+    };
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
+    const { post } = this.props;
+
+    if (!post) return <div>Loading...</div>;
+
     return (
-      <div>PostsShow</div>
+      <div>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
     );
   }
 }
 
-export default PostsShow;
+const mapStateToProps = ({ posts }, ownProps) => {
+  return { post: posts[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
