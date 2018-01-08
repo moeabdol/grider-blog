b221d3fdbbe259e662924df8f65d185fa2ec5c81
moeabdol <mohd.a.saed@gmail.com>
2018-01-08 17:34:34 +0300

    Add links to posts index and show pages

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions';
+import { Link } from 'react-router-dom';
 
 class PostsShow extends Component {
   static get propTypes() {
@@ -24,6 +25,7 @@ class PostsShow extends Component {
 
     return (
       <div>
+        <Link to="/">Back to Index</Link>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
